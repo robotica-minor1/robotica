0ea90e8d3ccd0fbac2b0ffdbbe6536560e90df06
Rutger van den Berg <rutgervandenberg@hotmail.com>
2015-01-20 12:53:26 +0100

    Clean up drone to remove arduino/servo code.

--- flightcontroller/include/drone.hpp
+++ flightcontroller/include/drone.hpp
@@ -1,27 +1,9 @@
 #ifndef DRONE_HPP_
 #define DRONE_HPP_
 #include <Eigen/Dense>
-// #include "VarSpeedServo.h"
+
 class Drone {
 private:
-	//servo settings
-	int servoPins[4] = {3, 6, 9, 10};
-	// VarSpeedServo servos[4];
-	int servoSpeeds[4] = {servoMaxSpeed, servoMaxSpeed, servoMaxSpeed, servoMaxSpeed};
-	int servoMinSpeed = 0; 
-	int servoMaxSpeed = 127;
-	int servoStartSpeed = 127;
-	
-	int servoMinAngle = 0;
-	int servoMaxAngle = 90;
-	int servoMinPulse = 1000;
-	int servoMaxPulse = 2000;
-
-	int servoOffset24 = 20;
-	int servoOffset13 = 70;
-
-
-	void setupServos();
 	void updateReferenceThrust(float gain, int signs[4]);
 public: 
 	Drone();
@@ -32,7 +14,6 @@ public:
 
 	//body surface area(assumed)
 	float surfaceArea = 0.06;
-
 	Eigen::Vector3f position;
 	Eigen::Vector3f landingSpot;
 	float distanceToLandingSpot;
@@ -40,6 +21,8 @@ public:
 	int motorRotationSigns[4] = {-1, 1, -1, 1};
 	Eigen::Vector3f velocity;
 	Eigen::Vector3f acceleration;
+	Eigen::Vector3f referenceAttitude;	
+
 
 };
 
--- flightcontroller/src/drone.cpp
+++ flightcontroller/src/drone.cpp
@@ -3,37 +3,11 @@
 #include "drone.hpp"
 // #include "VarSpeedServo.h"
 Drone::Drone() {
-	setupServos();
-}
-
-void Drone::setupServos() {
-	// servos[0].write(servoMinPulse+((servoMinPulse/(servoMaxAngle))*servoOffset13), servoStartSpeed);
-	// servos[1].write(servoMinPulse+((servoMinPulse/(servoMaxAngle))*servoOffset24), servoStartSpeed); 
-	// servos[2].write(servoMinPulse+((servoMinPulse/(servoMaxAngle))*servoOffset13), servoStartSpeed);
-	// servos[3].write(servoMinPulse+((servoMinPulse/(servoMaxAngle))*servoOffset24), servoStartSpeed);
 	
 }
 
 void Drone::setAxisRotation(Eigen::Vector4f axisAngles) {
 
-	//dirty hack to prevent odd behavior by the servos
-	// for (int i = 0; i < 4; i++) {
-	// 	servos[i].attach(servoPins[i], servoMinPulse, servoMaxPulse);
-	// }
-	
-	//compensate for the difference between desired angle and the angle 'known' by the servo
-	axisAngles[0] = fmin(fmax(axisAngles[0], servoMinAngle-(servoMaxAngle-servoOffset13)), servoOffset13);
-	axisAngles[2] = fmin(fmax(axisAngles[2], servoMinAngle-(servoMaxAngle-servoOffset13)), servoOffset13);
-
-	axisAngles[1] = fmin(fmax(axisAngles[1], servoMinAngle-servoOffset24), servoMaxAngle-servoOffset24);
-	axisAngles[3] = fmin(fmax(axisAngles[3], servoMinAngle-servoOffset24), servoMaxAngle-servoOffset24);
-
-	//write the new angles to the servos
-	// servos[0].write(((axisAngles[0]*-1+servoOffset13)*2), servoSpeeds[0]);
-	// servos[2].write(((axisAngles[2]*-1+servoOffset13)*2), servoSpeeds[2]);
-	
-	// servos[1].write(((axisAngles[1]*+servoOffset24)*2), servoSpeeds[1]);
-	// servos[3].write(((axisAngles[3]*+servoOffset24)*2), servoSpeeds[3]);
 }
 void Drone::setThrust(Eigen::Vector4f thrust) {
 	
